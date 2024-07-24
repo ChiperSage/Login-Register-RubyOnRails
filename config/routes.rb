a93a5b0c0b1d3d8862f2d6999871ce9200d12048
ByteNinja <pijayblog@gmail.com>
2024-07-24 20:26:16 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,12 @@
-.
+Rails.application.routes.draw do
+  root 'users#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'registrations#new'
+  post 'signup', to: 'registrations#create'
+
+  resources :users, except: [:new, :create]
+end
