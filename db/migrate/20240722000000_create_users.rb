bf3389e1f2297fe6b426f5b30d769e4c997637c2
ByteNinja <pijayblog@gmail.com>
2024-07-25 10:48:31 +0700

    Update 20240722000000_create_users.rb

--- db/migrate/20240722000000_create_users.rb
+++ db/migrate/20240722000000_create_users.rb
@@ -1,1 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email, null: false
+      t.string :password_digest
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
 
